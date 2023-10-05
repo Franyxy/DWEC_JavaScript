12a51581724254e7e7aa863b952ac31f515bacf9
Franyxy <117320622+Franyxy@users.noreply.github.com>
2023-10-05 19:36:11 +0200

    Update Ej06.js

--- P05/Ej06/Ej06.js
+++ P05/Ej06/Ej06.js
@@ -4,21 +4,18 @@ function comienzo(){
     document.formulario.boton.onclick=cadena;
 }
 
-function cadena(){
-    var VstCadena=document.formulario.cadena.value;
-    var VstSubcadena=document.formulario.subcadena.value;
-    var VstMensaje="";
+function cadena() {
+    var VstCadena = document.formulario.cadena.value;
+    var VstSubcadena = document.formulario.subcadena.value;
+    var VstMensaje = "";
+    var VarrVeces = VstCadena.match(new RegExp(VstSubcadena, 'g'));
 
-    if(VstCadena.indexOf(VstSubcadena)){
-        VstMensaje="La subcadena SI aparece en la cadena.";
-    }else{
-        VstMensaje="La subcadena NO aparece en la cadena principal.";
+    if (VarrVeces !== null) {
+        var VintAux = VarrVeces.length;
+        VstMensaje = "La subcadena SI aparece en la cadena, aparece " + VintAux + " veces";
+    } else {
+        VstMensaje = "La subcadena NO aparece en la cadena principal.";
     }
 
-    
-
-
-    
-    document.formulario.mensaje.value=VstMensaje;
-
+    document.formulario.mensaje.value = VstMensaje;
 }
